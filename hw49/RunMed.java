bf40596a3b274454415225c7224f9ea206bd4b73
Your Name <you@example.com>
2018-05-17 09:26:15 -0400

    hw49

---
+++ hw49/RunMed.java
@@ -0,0 +1,10 @@
+public class RunMed {
+    //inst vars
+    ALHeap<Integer> lilVals, bigVals;
+
+    //Constructor
+    public RunMed() {
+	lilVals = new ALHeap<Integer>();
+	bigVals = new ALHeap<Integer>();
+    }
+}
