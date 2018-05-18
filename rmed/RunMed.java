301b75c31bf659166936b0091ab752a87c10694e
Jude Grodesky <jgrodesky@stuy.edu>
2018-05-17 23:03:41 -0400

    RunMed add()

--- rmed/RunMed.java
+++ rmed/RunMed.java
@@ -24,4 +24,29 @@ public class RunMed {
 		return bigVals.peekMin(); }
 	}
     }
+    public void add(int addend) {
+
+	//Compare the addend to the current median and place it into its repective heap:
+	//lilVals for < , bigVals for >
+
+	//Check if there will be a median
+	if (lilVals.size() == 0 && bigVals.size() == 0) {
+	    lilvals.add(addend);
+	}
+	else if (addend <= getMedian()){
+	    lilVals.add(addend);
+	}
+	else if (addend > getMedian()){
+	    bigVals.add(addend);
+	}
+
+	//If heaps are to far apart in size, redistribute to balance
+	
+	if (lilVals.size() - bigVals.size() > 1){
+	    bigVals.add( lilVals.removeMax() );
+	}
+	else if (bigVals.size() - lilVals.size() > 1){
+	    lilVals.add( bigVals.removeMin() );
+	}
+    }
 }
